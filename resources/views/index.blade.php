59b1f4c0b4d37dcdd3ee78675f1e30b4a5956861
ElimuStuart <stuartelimu@gmail.com>
2020-03-04 07:09:48 +0300

    add index page to act as welcome page

---
+++ resources/views/index.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+
+@section('content')
+
+<section class="jumbotron text-center mt-0">
+    <div class="container">
+      <h1>Water Bursts </h1>
+      <p class="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.</p>
+
+      create an account or login to register a complaint with us.
+      <p>
+        <a href="#" class="btn btn-primary my-2">About Us</a>
+        <a href="#" class="btn btn-secondary my-2">Contact Us</a>
+      </p>
+    </div>
+</section>
+
+@endsection
